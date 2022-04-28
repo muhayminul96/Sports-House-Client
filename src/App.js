7dd2711760c49eb71609f1ac5263c52bd69caccf
muhayminul <muhayminul96@gmail.com>
2022-04-29 03:44:38 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import NotFoundError from './Pages/NotFoundError/NotFoundError';
+import Header from './Pages/shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path='*' element={<NotFoundError></NotFoundError>}></Route>
       </Routes>
