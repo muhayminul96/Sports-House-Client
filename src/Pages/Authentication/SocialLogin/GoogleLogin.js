8500a513a99254ac1fc605f0b8dac26f009bd0ba
muhayminul <muhayminul96@gmail.com>
2022-04-29 14:57:51 +0600

    social google login enabled

--- src/Pages/Authentication/SocialLogin/GoogleLogin.js
+++ src/Pages/Authentication/SocialLogin/GoogleLogin.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import auth from '../../../firebaseInit'
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import Loading from '../../shared/Loadig/Loading';
 
 
 const GoogleLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    const handleGoogle = () => {
-        console.log(auth)
+    if(loading){
+        return <Loading></Loading>
     }
+   if(user){
+       console.log(user)
+   }
     return (
         
         <div>
-            <button onClick={handleGoogle}>click</button>
+            <button className='btn btn-dark' onClick={() => {signInWithGoogle()}}>
+            Sign With Google
+            </button>
         </div>
     );
 };
